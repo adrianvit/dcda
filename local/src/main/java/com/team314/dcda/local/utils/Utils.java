419bd8e347b9726119222282af933f4d32dc1d83
adrianvit <adrianvit@gmail.com>
2013-05-25 23:56:04 +0300

    web.xml fix + utils in progress

--- local/src/main/java/com/team314/dcda/local/utils/Utils.java
+++ local/src/main/java/com/team314/dcda/local/utils/Utils.java
@@ -5,8 +5,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -15,13 +20,24 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.HttpParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.jersey.api.client.AsyncWebResource;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.client.non.blocking.NonBlockingClient;
+import com.sun.jersey.client.non.blocking.config.DefaultNonBlockingClientConfig;
+import com.sun.jersey.client.non.blocking.config.NonBlockingClientConfig;
 import com.team314.dcda.local.dao.LoggedUserDAO;
+import com.team314.dcda.local.db.Peer;
+import com.team314.dcda.local.db.Product;
 
 
 public class Utils {
@@ -29,8 +45,8 @@ public class Utils {
 	public static final String local_ip_jndi_name = "LocalIp";
 	public static final String local_name_jndi_name = "LocalName";
 	public static final String central_ip = "localhost";
-	public static final String central_path = "central/locate";
-	public static final String central_path_register = "central/register";
+	public static final String central_path = "central/rest/locate";
+	public static final String central_path_register = "central/rest/register";
 	public static final String scheme  = "http";
 	public static final int central_port = 18080;
 	private static final Logger LOG = LoggerFactory.getLogger(Utils.class);
@@ -165,4 +181,38 @@ public class Utils {
 		}
 		return false;
 	}
+	
+	
+	public static List<Product> searchForProductsInPeer(Peer peer, String key, int ttl)
+	{
+		UriBuilder uriBuilder = UriBuilder.fromUri("/local");
+		//uriBuilder.scheme("http").host(peer.getUrl()).port(8080);
+		uriBuilder.scheme("http").host("localhost").path("/search").queryParam("key", key).queryParam("ttl", Integer.toString(ttl-2)).port(18080);
+		URI uri = uriBuilder.build();
+		try
+		{
+			 ClientConfig cc2 = new DefaultClientConfig();
+			 cc2.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+			 Client c2 = Client.create(cc2);
+			 WebResource wr = c2.resource(uri);
+			 String temp= wr.accept(MediaType.APPLICATION_JSON).get(String.class);
+			 
+			 ClientConfig cc = new DefaultNonBlockingClientConfig();
+			 cc.getProperties().put(NonBlockingClientConfig.PROPERTY_THREADPOOL_SIZE, 10);
+			 cc.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+			 Client c = NonBlockingClient.create(cc);
+			 c.setConnectTimeout(1000);
+			 AsyncWebResource awr = c.asyncResource(uri);
+			 Future<List<Product>> responseFuture = awr.accept(MediaType.APPLICATION_JSON).get(new GenericType<List<Product>>(){});
+			 return responseFuture.get(2, TimeUnit.SECONDS);		
+		}catch(Exception e)
+		{
+			LOG.error("Error searching for products", e);
+			return null;
+		}
+	}
+	
+	 public static void main(final String[] args) throws Exception {
+
+	 }
 }
