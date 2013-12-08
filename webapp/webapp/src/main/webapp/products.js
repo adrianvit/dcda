2b063765ca850326a6de2ddf3dcd95b25ae12b62
unknown <aletoderescu@gmail.com>
2013-12-08 22:26:46 +0100

    second

---
+++ webapp/webapp/src/main/webapp/products.js
@@ -0,0 +1,7 @@
+function Product(product_id, description, price, quantity) {
+				var self = this;
+				self.product_id = product_id;
+				self.description = description;
+				self.price = price;
+				self.quantity = quantity;
+			};
